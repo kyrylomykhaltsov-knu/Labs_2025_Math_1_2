fd64845b951f85794eb2e7539b50741012f056aa
Kyrylo Mykhaltsov <kyrylo.mykhaltsov@knu.ua>
2025-09-10 13:40:54 +0300

    Test

--- L02/L03.py
+++ L02/L03.py
@@ -1,2 +1,13 @@
-n=int(input())
+n = int(input())
 print("YES" if n%2==1 or (99<n and n<1000) else "NO")
+
+condition_1 = n % 2 == 1
+condition_2 = 99 < n < 1000
+condition = condition_1 or condition_2
+
+if condition:
+    print("YES")
+else:
+    print("NO")
+
+pass

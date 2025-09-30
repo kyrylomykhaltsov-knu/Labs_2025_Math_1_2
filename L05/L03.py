6a2b7a36baa880f64c8a8fd1b4abb41bec7d7b72
m-kyrylo <mikhaltsov.kirill@gmail.com>
2025-10-01 02:03:27 +0300

    L05

---
+++ L05/L03.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+a = [float(el) for el in input().split()]
+
+sum = 0
+counter = 0
+
+for i in range(2, N, 3):
+    if a[i] > 0:
+        counter += 1
+        sum += a[i]
+
+print(counter, "%0.2f" % sum, sep = " ")
